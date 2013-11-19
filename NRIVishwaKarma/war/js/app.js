9c9bf065318c19ac20788871626de0736014c33a
Katrina Lander <katrina.lander@gmail.com>
2013-11-19 15:12:48 -0600

    Katrina Lander created newUser.html page

--- NRIVishwaKarma/war/js/app.js
+++ NRIVishwaKarma/war/js/app.js
@@ -1,7 +1,32 @@
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp',['$strap.directives']);
 
 app.config(function($routeProvider){
 	/*$routeProvider.
 	when('/',{templateUrl:'/',controller:''}).
 	otherwise({redirecTo:'/',template:'/',controller:''});*/
 });
+
+app.controller('newUserCtrl',function($scope, $window, $location){
+
+	  // Datepicker directive
+	  $scope.datepicker = {date: new Date()};
+	  
+	  
+	  
+	});
+
+	// Docs styles
+	//
+	$(function() {
+	  $("html").removeClass("no-js").addClass("js");
+		var $window = $(window);
+	  new FastClick(document.body);
+		$(document).ready(function($) {
+			// Disable certain links in docs
+			$('section [href^=#]').click(function (e) {
+				e.preventDefault();
+			});
+			// Make code pretty
+			window.prettyPrint && window.prettyPrint();
+		});
+	});
